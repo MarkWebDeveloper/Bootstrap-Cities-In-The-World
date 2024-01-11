63bfd041d7cd523e64fe737e842e2b25128c3f66
Mark Kil <markkilcanarias@gmail.com>
2024-01-11 21:22:26 +0100

    header is now a module

---
+++ src/js/header.js
@@ -0,0 +1,28 @@
+let header = document.querySelector('header')
+
+export function loadHeader() {
+    header.innerHTML = /* html */ `
+    <nav class="navbar navbar-expand-lg navbar-light px-3 my-navbar">
+        <a class="navbar-brand my-header-text" href="#">Cities in the world</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item active">
+                    <a class="nav-link my-header-text" href="#">Home</a>
+                </li>
+                <p class="my-auto">-</p>
+                <li class="nav-item">
+                    <a class="nav-link my-header-text" href="#">About</a>
+                </li>
+                <p class="my-auto">-</p>
+                <li class="nav-item">
+                    <a class="nav-link my-header-text" href="#">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    `
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,11 @@
 import '../scss/styles.scss'
+import '../scss/general-rules.scss'
 import '../scss/header.scss'
 import * as bootstrap from 'bootstrap'
+import { loadHeader } from './header'
+
+function app() {
+    loadHeader()
+}
+
+app()
