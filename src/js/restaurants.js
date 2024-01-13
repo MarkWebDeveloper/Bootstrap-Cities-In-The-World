992fd0ad6f7d9a0c6df9f6f5ef8ef04dbbcc420a
Mark Kil <markkilcanarias@gmail.com>
2024-01-13 20:42:06 +0100

    best reastaurants added

--- src/js/restaurants.js
+++ src/js/restaurants.js
@@ -3,22 +3,22 @@ let restaurants = document.querySelector('#restaurants-section')
 export function loadRestaurants() {
     restaurants.innerHTML = /* html */ `
     <h3 class="my-restaurants-title h3 text-center">Most Popular Restaurants</h3>
-    <div class="my-restaurants-container d-flex align-items-center">
+    <div class="my-restaurants-container d-flex align-items-center justify-content-evenly">
         <div class="my-restaurant-container d-flex flex-column align-items-center justify-content-evenly">
-            <div class="my-restaurant-image"></div>
-            <p class="my-restaurant-description m-0 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <div class="my-central my-restaurant-image"></div>
+            <p class="my-restaurant-description m-0 text-center">Central Lima</p>
         </div>
         <div class="my-restaurant-container d-flex flex-column align-items-center justify-content-evenly">
-            <div class="my-restaurant-image"></div>
-            <p class="my-restaurant-description m-0 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <div class="my-disfrutar my-restaurant-image"></div>
+            <p class="my-restaurant-description m-0 text-center">Disfrutar Barcelona</p>
         </div>
         <div class="my-restaurant-container d-flex flex-column align-items-center justify-content-evenly">
-            <div class="my-restaurant-image"></div>
-            <p class="my-restaurant-description m-0 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <div class="my-diverxo my-restaurant-image"></div>
+            <p class="my-restaurant-description m-0 text-center">Divertxo Madrid</p>
         </div>
         <div class="my-restaurant-container d-flex flex-column align-items-center justify-content-evenly">
-            <div class="my-restaurant-image"></div>
-            <p class="my-restaurant-description m-0 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <div class="my-asador-etxebarri my-restaurant-image"></div>
+            <p class="my-restaurant-description m-0 text-center">Asador Etxebarri Atxondo</p>
         </div>
     </div>
     `
