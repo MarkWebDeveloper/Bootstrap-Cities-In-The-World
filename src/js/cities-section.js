1773068358dae867fb4b7a24e271f3aac3ea1b48
Mark Kil <markkilcanarias@gmail.com>
2024-01-14 14:33:51 +0100

    company logo added

--- src/js/cities-section.js
+++ src/js/cities-section.js
@@ -30,14 +30,14 @@ export function loadCities() {
                 <div class="my-city-container d-flex flex-column" >
                     <div id="hong-kong" class="hong-kong-photo my-city-photo"></div>
                     <div class="p-2">
-                        <p class="my-city-description-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <p class="my-city-description-text m-0"><b>Hong Kong</b> - city and a special administrative region in China.</p>
                     </div>
                     <button class="my-city-info-button w-50 align-self-end m-2">INFO</button>
                 </div>
                 <div class="my-city-container d-flex flex-column" >
                     <div class="singapore-photo my-city-photo"></div>
                     <div class="p-2">
-                        <p class="my-city-description-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <p class="my-city-description-text m-0"><b>Singapore</b> -  island country and city-state in maritime Southeast Asia.</p>
                     </div>
                     <button class="my-city-info-button w-50 align-self-end m-2">INFO</button>
                 </div>
@@ -46,14 +46,14 @@ export function loadCities() {
                 <div class="my-city-container d-flex flex-column" >
                     <div class="bangkok-photo my-city-photo"></div>
                     <div class="p-2">
-                        <p class="my-city-description-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <p class="my-city-description-text m-0"><b>Bangkok</b> - the capital and most populous city of Thailand.</p>
                     </div>
                     <button class="my-city-info-button w-50 align-self-end m-2">INFO</button>
                 </div>
                 <div class="my-city-container d-flex flex-column" >
                     <div class="macau-photo my-city-photo"></div>
                     <div class="p-2">
-                        <p class="my-city-description-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <p class="my-city-description-text m-0"><b>Macau</b> - a city and special administrative region of China in the western Pearl River Delta.</p>
                     </div>
                     <button class="my-city-info-button w-50 align-self-end m-2">INFO</button>
                 </div>
@@ -62,14 +62,14 @@ export function loadCities() {
                 <div class="my-city-container d-flex flex-column" >
                     <div class="london-photo my-city-photo"></div>
                     <div class="p-2">
-                        <p class="my-city-description-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <p class="my-city-description-text m-0"><b>London</b> - the capital and largest city of England and the United Kingdom.</p>
                     </div>
                     <button class="my-city-info-button w-50 align-self-end m-2">INFO</button>
                 </div>
                 <div class="my-city-container d-flex flex-column" >
                     <div class="paris-photo my-city-photo"></div>
                     <div class="p-2">
-                        <p class="my-city-description-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <p class="my-city-description-text m-0"><b>London</b> - the capital and most populous city of France.</p>
                     </div>
                     <button class="my-city-info-button w-50 align-self-end m-2">INFO</button>
                 </div>
--- src/js/footer.js
+++ src/js/footer.js
@@ -7,7 +7,7 @@ export function loadFooter() {
                 <div class="my-footer-line"></div>
                 <div class="my-footer-underline-container w-100 h-100 d-flex align-items-center">
                     <div class="my-company-logo"></div>
-                    <p class="my-copyright-text my-0 mx-2">© 2021 Company, Inc</p>
+                    <p class="my-copyright-text my-0 mx-2">© 2021 Mountains Travel, Inc</p>
                     <div class="my-icons-container d-flex justify-content-between align-items-center">
                         <img class="my-social-logos" src="/images/logos/icons8-facebook-100.png" alt="facebook">
                         <img class="my-social-logos" src="/images/logos/icons8-instagram-120.png" alt="facebook">
