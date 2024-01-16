a430f67ef525232719b8c81fa8b021dff5a5c85f
Mark Kil <markkilcanarias@gmail.com>
2024-01-16 19:19:01 +0100

    publishing on gh-pages3

--- assets/index-ss-W5u_e.js
+++ assets/index-ss-W5u_e.js
@@ -127,9 +127,9 @@
                     <div class="my-company-logo"></div>
                     <p class="my-copyright-text my-0 mx-2">© 2021 Mountains Travel, Inc</p>
                     <div class="my-icons-container d-flex justify-content-between align-items-center">
-                        <img class="my-social-logos" src="/images/logos/icons8-facebook-100.png" alt="facebook">
-                        <img class="my-social-logos" src="/images/logos/icons8-instagram-120.png" alt="facebook">
-                        <img class="my-social-logos" src="/images/logos/icons8-twitterx-100.png" alt="facebook">
+                        <img class="my-social-logos" src="./images/logos/icons8-facebook-100.png" alt="facebook">
+                        <img class="my-social-logos" src="./images/logos/icons8-instagram-120.png" alt="facebook">
+                        <img class="my-social-logos" src="./images/logos/icons8-twitterx-100.png" alt="facebook">
                     </div>
                 </div>
             </div>
